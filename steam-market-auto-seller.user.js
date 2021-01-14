ba6f712e72dec52645ca5b56b9a1b6eae15ee066
Ramin Quluzade <ramin.quluzade@eurodesign.az>
2021-01-14 18:04:41 +0400

    updated @match URL condition

--- steam-market-auto-seller.user.js
+++ steam-market-auto-seller.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Steam Market AutoSeller
 // @description  Adds new "AutoSell" button near "Sell" to set selling price, tick SSA checkbox, confirm sale & close dialog automatically. Also trims too long card descriptions & hides "Gems" section.
-// @version      1.0.7
+// @version      1.0.8
 // @author       Silmaril
 // @namespace    https://github.com/SOLiNARY
 // @downloadURL  https://raw.githubusercontent.com/SOLiNARY/steam-market-auto-seller/master/steam-market-auto-seller.user.js
 // @updateURL    https://raw.githubusercontent.com/SOLiNARY/steam-market-auto-seller/master/steam-market-auto-seller.meta.js
 // @license      MIT License
 // @copyright    Copyright (C) 2020, by Silmaril
-// @match        *://steamcommunity.com/id/*/inventory
+// @match        *://steamcommunity.com/id/*/inventory*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js
 // @grant        none
 // ==/UserScript==
